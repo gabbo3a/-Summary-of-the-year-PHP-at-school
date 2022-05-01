5e339c9a7f31893aa236931146d6928bb03a3686
s5779870b <s5779870b@studenti.itisavogadro.it>
2022-05-01 19:24:37 +0200

    little change

--- exercises/ES1/index.php
+++ exercises/ES1/index.php
@@ -1,57 +1,37 @@
 <!DOCTYPE html>
 <meta http-equiv="refresh" content="1">
-
 <head>
-    <!-- Charset and viewport  -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Link exterbal css-->
-    <link rel="stylesheet" href="./style.css">
-
-    <title>Request Date</title>
+    <link rel="stylesheet" href="./style.css">  <!-- Link exterbal css -->
+    <title>Clock</title>
 </head>
+<html>
 <body>
+    <?php 
+        //Hard code data
+        $weekSet = array("Domenica", "Lunedi", "Martedi", "Mercoledi", "Giovedi", "Venerdi", "Sabato");
+        $monthsSet = array("Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giunio", "Luglio", "Agosto", "Setterbre", "Ottobre", "Novenmbre", "Dicembre");
+    ?>   
 
-    
-        <!-- <div class="father"> -->
-        <?php 
-            //Hard code data
-            $weekSet = array("Lunedi", "Martedi", "Mercoledi", "Giovedi", "Venerdi", "Sabato", "Domenica");
-            $monthsSet = array("Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giunio", "Luglio", "Agosto", "Setterbre", "Ottobre", "Novenmbre", "Dicembre");
-
-            $dayStamp = 1;
-            $mounthStamp= 1;
-        ?>   
-        <div>
-            <!-- Date Card -->
-            <div class="calendar-day">
-
-                <!-- Print Months -->
-                <div class="month">
-                    <?php echo $monthsSet[date("n")-1]; ?>
-                </div>
-
-                <!-- Print Day -->
-                <div class="day">
-                    <div class="number-day">
-                        <?php echo date("j"); ?>
-                    </div>
-                    <div class="literals-day">
-                        <?php echo $weekSet[date("w")-1]; ?>
-                    </div>
-                </div>
-
+    <!-- Date Card -->
+    <div class="calendar-day">
+        <div class="month"> <!-- Print Months -->
+            <?php echo $monthsSet[date("n")-1]; ?>
+        </div>
+        <div class="day"> <!-- Print Day -->
+            <div class="number-day">
+                <?php echo date("j"); ?>
             </div>
-
-            <!-- Time Card -->
-            <div class="time">
-                <?php echo  date("H:i:s"); ?>
+            <div class="literals-day">
+                <?php echo $weekSet[date("w")]; ?>
             </div>
-       <!--  </div> -->
-        <!-- <button>Update date attivare il refrafh ayto</button> --->
-        
+        </div>
+        <div class="time"> <!-- Time Card -->
+            <?php echo date("H:i:s"); ?>
+        </div>
     </div>
+    
     <script src="./js/script.js"></script>
 </body>
 </html>
--- exercises/ES2/index.php
+++ exercises/ES2/index.php
@@ -1,45 +1,36 @@
 <!DOCTYPE html>
 <head>
-
-    <!-- Charset and viewport  -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Link exterbal css-->
-    <link rel="stylesheet" href="./style.css">
-
+    <link rel="stylesheet" href="./style.css"> <!-- Link exterbal css-->
     <title>Translate</title>
 </head>
+<html>
 <body>
-
-    <?php    
-        //Hard coding data
-        $words = array("cane"=>"dog", "gatto"=>"cat", "pane"=>"bread", "tastiera"=>"keyboard", "zaino"=>"bag");
+    <?php // Hard coding data
+        $words = array(
+            "cane"=>"dog", 
+            "gatto"=>"cat", 
+            "pane"=>"bread", 
+            "tastiera"=>"keyboard", 
+            "zaino"=>"bag"
+        );
     ?>
-
     <div class="container"> 
         <form action="index.php" method="get">
             <select name="word">
-            
-                <?php
-                    //Dynamic option
-                    foreach($words as $word => $value)
-                        echo ("<option value=\"$value\">$word</option>");
-                ?>
+                <?php foreach($words as $word => $value) ?>
+                <option value=<?php echo $value;?>> <?php echo $word; ?></option>      
             </select>
-
             <button type="submit">Invia</button>
         </form>
-
-
-        <?php  if (isset($_GET['word'])):/*<Se $_GET['word'] è settata stampa*/ ?>    
+        
+        <?php  if (isset($_GET['word'])): // Se $_GET['word'] è settata stampa ?>    
             <div class="information-box">
-                    La tua parola = <?php echo $_GET['word'];/*Array super globale get*/ ?> 
+                La tua parola = <?php echo $_GET['word'];// Array super global get ?> 
             </div>
         <?php endif;?>
     </div>     
-
-    <!-- -->
     <script src="./js/script.js"></script>
 </body>
 </html>
